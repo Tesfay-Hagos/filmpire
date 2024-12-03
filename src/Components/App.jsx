61b74ded875335a3edc41e41835d606da4dfd469
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2024-12-03 16:42:38 +0300

    Add MUI components and fix typo in App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { cssBaseline} from '@mui/material'
+import {Router, swithc} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+    <cssBaseline />
       <h1>Hello FilmPire</h1>
     </div>
   )
