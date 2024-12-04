dfea85c7498b68d3dd85f23be91295530bbf62c4
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2024-12-04 07:47:01 +0300

    Enhance NavBar component with search functionality and authentication button

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import { AppBar,IconButton,Toolbar,Drawer,Button,Avatar,useMediaQuery } from '@mui/material'
-import {Menu,AccountCircle,Brightness4,Brightness7} from '@mui/icons-material'
+import { AppBar,IconButton,Toolbar,
+  Drawer,Button,Avatar,useMediaQuery,
+  useTheme } from '@mui/material'
+import {Menu,AccountCircle,
+  Brightness4,Brightness7,
+  Search} from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import useStyles from './styles'
  
@@ -8,24 +12,51 @@ import useStyles from './styles'
 const NavBar = () => {
   const classes = useStyles()
   const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
   return (
-    <>
-      <AppBar position='fixed'>
-        <Toolbar className={classes.toolbar}>
-        {isMobile && (
-          <IconButton
+    <AppBar position='fixed'>
+    <Toolbar className={classes.toolbar}>
+      {isMobile && (
+        <IconButton
+        color='inherit'
+        edge='start'
+        style={{outline: 'none'}}
+        onClick={() => {}}
+        className={classes.menuButton}
+        >
+          <Menu />
+        </IconButton>
+      )}
+      <IconButton color='inherit' sx={{ml:1}} onClick={()=>({})}>
+      {theme.palette.mode==='dark' ? <Brightness7 /> : <Brightness4 />}
+      </IconButton>
+      {!isMobile && 'Search...'}
+      <div>
+        {!isAuthenticated?(
+          <Button color='inherit' onClick={()=> ({})}>
+            Login &nbsp;
+          </Button>
+        ):(
+          <Button 
           color='inherit'
-          edge='start'
-          style={{outline: 'none'}}
-          onClick={() => {}}
-          className={classes.menuButton}
+          component={Link}
+          to='/profile/:id'
+          className={classes.linkButton}
+          onClick={()=>({})}
           >
-            <Menu />
-          </IconButton>
+            {!isMobile && <>My Movies nbsb &nbsp;</>}
+            <Avatar 
+            style={{width:30,height:30}}
+            alt='profile' 
+            src='https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg' 
+            />
+          </Button>
         )}
-        </Toolbar>
-      </AppBar>
-    </>
+      </div>
+      {isMobile && 'Search...'}
+      </Toolbar>
+    </AppBar>
   )
 }
 
